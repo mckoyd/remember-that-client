103c21ff90e4e9449d38cd441878560853b60476
mckoyd <dominick.mckoy@gmail.com>
2018-06-08 15:11:44 -0400

    Fixed form layout

--- src/components/LTopNav.js
+++ src/components/LTopNav.js
@@ -6,6 +6,6 @@ export const LTopNav = () => (
     <img src={logo}
       alt="RememberThat! logo, a post-it note with Remember That written in bold."
       onClick={() => console.log('`RememberTHAT!` icon link ready')}/>
-    <a href="#ious" onClick={() => console.log('`RememberTHAT!` text link ready.')}>Remember THAT!</a>
+    <a href="#" onClick={() => console.log('`RememberTHAT!` text link ready.')}>Remember THAT!</a>
   </div>
 );
